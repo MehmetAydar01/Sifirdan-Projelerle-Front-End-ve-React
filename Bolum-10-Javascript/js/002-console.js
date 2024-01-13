7de19f2a63cb487691f56c5a9e315eae69d5233a
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-13 04:25:51 +0300

    log konusu

---
+++ Bolum-10-Javascript/js/002-console.js
@@ -0,0 +1,14 @@
+console.count('count ?');
+console.log(console);
+console.count('count ?');
+console.error('hata bilgisi');
+
+const user = {
+  firstName: 'Hakan',
+  lastName: 'Yalcinkaya',
+};
+
+console.log('log:', user);
+console.table(user);
+
+console.info('bilgi');
