f0679b8376f710501ecbe3fb94bef7f951118eab
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-13 04:25:56 +0300

    variables

---
+++ Bolum-10-Javascript/js/003-variable.js
@@ -0,0 +1,10 @@
+let API_KEY = 'FJSDNFJSDNFJNDSJFNDSJNFDSÖMFDKLSMFKSDMFKDSFMSD';
+
+console.log(API_KEY);
+
+// firstName -> camelCase
+// FirstName ????
+
+// Degisken isimlerinin dogru kullanilmasi
+let firstName = 'Dogu';
+console.log(firstName);
