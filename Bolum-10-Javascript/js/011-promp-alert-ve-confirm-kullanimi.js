3153a170ee036c706a8c3d2c687081c0017fa003
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-14 23:18:35 +0300

    alert prompt confirm methodları

---
+++ Bolum-10-Javascript/js/011-promp-alert-ve-confirm-kullanimi.js
@@ -0,0 +1,15 @@
+// Prompt:
+
+// const info = prompt('En sevdiginiz yazilim dili hangisi', 'Python');
+
+// Alert:
+// alert(info);
+
+// console.log(info);
+
+// Confirm:
+// console.log(confirm('Kullanici Silinsin mi'));
+
+// if (confirm('Kullanici Silinsin mi')) {
+//   console.log('kullanici silindi');
+// }
