7f854dbb143f15e2f5cf5812c5af0226aa29f332
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-14 23:18:22 +0300

    veri turu donusumleri

---
+++ Bolum-10-Javascript/js/010-veri-turu-donusumleri.js
@@ -0,0 +1,33 @@
+let isActive = true;
+let firstName = 'Beyza';
+let year = 2024;
+
+console.log('isActive', isActive, typeof isActive);
+console.log('firstName', firstName, typeof firstName);
+console.log('year', year, typeof year);
+console.log('year bilgisi number mi ?', typeof year === 'number');
+
+let age = 35;
+let addToAge = '11.37abc';
+let result = age + addToAge;
+
+console.log('age + addToAge : ', result);
+console.log('typeof age : ', typeof age);
+console.log('typeof addToAge : ', typeof addToAge);
+console.log('typeof result : ', typeof result);
+
+// string icindeki bilgiyi parse etmeye calisiyoruz
+console.log(typeof addToAge, typeof parseInt(addToAge));
+console.log(addToAge, parseInt(addToAge));
+
+result = age + parseInt(addToAge);
+console.log('age + addToAge : ', result);
+
+result = age + Number(addToAge);
+console.log('age + addToAge : ', result);
+
+result = age + parseFloat(addToAge);
+console.log('age + addToAge : ', result);
+
+let strResult = result.toString();
+console.log('strResult typeof :', typeof strResult);
