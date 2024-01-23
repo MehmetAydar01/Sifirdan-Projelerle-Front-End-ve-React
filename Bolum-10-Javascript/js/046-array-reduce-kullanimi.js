a3358f8064805766532cecc1a595a7094af21295
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-23 21:43:00 +0300

    Array reduce kullanimi

---
+++ Bolum-10-Javascript/js/046-array-reduce-kullanimi.js
@@ -0,0 +1,42 @@
+// reduce kullanimi
+
+// arr.reduce((onceki, simdiki) => {yapilacak islem},  ilkDeger)
+
+const items = [1, 2, 3, 4, 5];
+
+const sum = items.reduce((accumulator, current) => {
+  return accumulator + current;
+}, 0);
+
+console.log(sum);
+
+const prices = [100, 200, 10, 40, 500, 90];
+
+const maxPrice = prices.reduce((acc, current) => {
+  return Math.max(acc, current);
+});
+console.log(maxPrice);
+
+const itemPrices = [
+  { name: 'Laptop', price: 10 },
+  { name: 'Ipad', price: 1022 },
+  { name: 'Smart Phone', price: 120 },
+  { name: 'Keyboard', price: 110 },
+  { name: 'Mouse', price: 104 },
+];
+
+const sumItemPrices = itemPrices.reduce((acc, current) => {
+  return acc + current.price;
+}, 0);
+console.log(sumItemPrices);
+
+const users = ['Lorem', 'Ipsum', 'Derya', 'Deniz', 'Ipsum', 'Deniz', 'Lorem'];
+
+const countedUsers = users.reduce((allUsers, current) => {
+  const count = allUsers[current] ?? 0;
+  return {
+    ...allUsers,
+    [current]: count + 1,
+  };
+}, {});
+console.log(countedUsers);
