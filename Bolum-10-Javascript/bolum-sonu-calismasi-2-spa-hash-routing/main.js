c36c2d8c0430c4f3e177060d005c429ba1d5dcad
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-24 19:35:45 +0300

    bolum sonu calismasi 2 hash routing olusturuldu

---
+++ Bolum-10-Javascript/bolum-sonu-calismasi-2-spa-hash-routing/main.js
@@ -0,0 +1,67 @@
+console.log('window.location.hash', window.location.hash);
+console.log('window.location.port', window.location.port);
+console.log('window.location.host', window.location.host);
+console.log('window.location.hostname', window.location.hostname);
+
+const hashLinks = document.querySelectorAll('.hash-route-link');
+
+const app = document.querySelector('#app');
+
+const routes = {
+  '': {
+    title: 'Ana Sayfa',
+    data: './home.html',
+    isApi: false,
+  },
+  '#todos': {
+    title: 'Todo List',
+    data: 'https://jsonplaceholder.typicode.com/todos',
+    isApi: true,
+  },
+  '#about': {
+    title: 'About Us',
+    data: './about-us.html',
+    isApi: false,
+  },
+  '#contact': {
+    title: 'Contact',
+    data: './contact.html',
+    isApi: false,
+  },
+};
+
+function checkRoute(hash = window.location.hash) {
+  // console.log(routes[hash]);
+  window.document.title = routes[hash].title;
+  const dataUrl = routes[hash].data;
+  const isApi = routes[hash].isApi;
+  console.log(dataUrl);
+  if (isApi) {
+    app.innerHTML = '';
+    const createUl = document.createElement('ul');
+    app.append(createUl);
+    fetch(dataUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach((item) => {
+          const createLi = document.createElement('li');
+          createLi.innerHTML = item.title;
+          createUl.appendChild(createLi);
+        });
+      });
+  } else if (!isApi) {
+    fetch(dataUrl)
+      .then((res) => res.text())
+      .then((data) => (app.innerHTML = data));
+  }
+}
+
+hashLinks.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    // console.log(item.hash);
+    // console.log(event);
+    checkRoute(item.hash);
+  });
+});
+
+checkRoute();
