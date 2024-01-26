af896244048455c070bae46b86c44c2f35e660fb
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-26 20:48:01 +0300

    users data created

---
+++ Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/data/users.js
@@ -0,0 +1,24 @@
+const USERS = [
+  'Lorem.',
+  'Dolorem.',
+  'Recusandae!',
+  'Inventore?',
+  'Libero!',
+  'Vitae!',
+  'Animi.',
+  'Officiis.',
+  'Ullam.',
+  'Iste.',
+  'Dolor.',
+  'Quos.',
+  'Eaque?',
+  'Omnis.',
+  'Vel.',
+  'Suscipit!',
+  'Aspernatur.',
+  'Repellat.',
+  'Impedit.',
+  'Voluptatem!',
+];
+
+export default USERS;
