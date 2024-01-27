b250dfc8deb8668a3c5693443200b29843e32f6d
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-27 23:03:51 +0300

    Button componentine props ile bilgi gonderildi

--- Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/components/Button.jsx
+++ Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/components/Button.jsx
@@ -1,5 +1,35 @@
-export default function Button() {
-  return <button>Button Component</button>;
+// export default function Button() {
+//   return <button>Button Component</button>;
+// }
+
+// 1.Yontem
+// export default function Button(props) {
+//   console.log(props);
+//   return (
+//     <button className={props.className} data-id={props.dataId}>
+//       {props.info}
+//     </button>
+//   );
+// }
+
+// 2.Yontem
+// export default function Button(props) {
+//   const { className, dataId, info } = props; // destructuring
+
+//   return (
+//     <button className={className} data-id={dataId}>
+//       {info}
+//     </button>
+//   );
+// }
+
+// 3.Yontem
+export default function Button({ className, dataId, info }) {
+  return (
+    <button className={className} data-id={dataId}>
+      {info}
+    </button>
+  );
 }
 
 // export default Button;
