4c86d92da29e49013d31080e148c650cbdd0faa4
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-30 23:02:21 +0300

    css style updated

--- Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/components/ImgThumbnail.jsx
+++ Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/components/ImgThumbnail.jsx
@@ -3,7 +3,7 @@ export default function ImgThumbnail({ imgSrc, imgAlt }) {
     padding: '0.25rem',
     borderRadius: '0.5rem',
     border: '0.1rem solid #bdc3c7',
-    width: '100%',
+    maxWidth: '100%',
     boxSizing: 'border-box',
     marginBottom: '5px',
   };
