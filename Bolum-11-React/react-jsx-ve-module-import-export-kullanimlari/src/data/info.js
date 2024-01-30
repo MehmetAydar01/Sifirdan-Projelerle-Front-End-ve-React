bfea066a6cef7d4ca98c3f1b87c333e4df3bc10e
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-30 23:02:44 +0300

    info data added

---
+++ Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/data/info.js
@@ -0,0 +1,16 @@
+// OPTIONAL CHAIN
+
+export const INFO = {
+  user: 'Lorem',
+  password: '122343',
+  productzzzzzz: [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8',
+  ],
+};
