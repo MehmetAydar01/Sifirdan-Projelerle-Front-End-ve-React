7121b92a8a0a9c660c3020ecfc89e3dc0a026481
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-01-31 23:39:02 +0300

    Card Footer component eklendi

---
+++ Bolum-11-React/react-jsx-ve-module-import-export-kullanimlari/src/components/CardWithState/CardFooter.jsx
@@ -0,0 +1,11 @@
+export default function CardFooter({ like, handleClick, setLike }) {
+  return (
+    <>
+      <h2>Card Footer ( {like} )</h2>
+      <button onClick={handleClick}>Footer {'->'} Like</button>
+      <button onClick={() => setLike((prev) => prev + 1)}>
+        Footer setLike ile degistir
+      </button>
+    </>
+  );
+}
