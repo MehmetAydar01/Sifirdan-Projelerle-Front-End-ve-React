8c39d1899e1db904474e31035897acd56b14582c
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-03 00:59:28 +0300

    componentlar export edildi

---
+++ Bolum-11-React/bolum-sonu-calismasi-v2/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ImgNotSelected } from './ImgNotSelected.jsx';
+export { default as ImgContainer } from './ImgContainer.jsx';
+export { default as ImgSelected } from './ImgSelected.jsx';
