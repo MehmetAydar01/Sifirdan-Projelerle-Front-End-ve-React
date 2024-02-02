37f59a46d8b75f35298230007387f38786e42fc1
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-03 00:59:06 +0300

    component olusturuldu

---
+++ Bolum-11-React/bolum-sonu-calismasi-v2/src/components/ImgNotSelected.jsx
@@ -0,0 +1,11 @@
+function ImgNotSelected() {
+  return (
+    <article>
+      <header>Img Secilmedi</header>
+      Lutfen Bir Gorsel Seciniz...
+      <footer>...</footer>
+    </article>
+  );
+}
+
+export default ImgNotSelected;
