807ab0e48782ddeca0ffe6827779717ace5ff33c
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-08 23:36:50 +0300

    ui duzenlendi, duzenle butonu eklendi

--- Bolum-12-React-Form-ile-Calismak/src/TaskList.jsx
+++ Bolum-12-React-Form-ile-Calismak/src/TaskList.jsx
@@ -1,29 +1,43 @@
-const TaskList = ({ tasks, removeTask }) => {
+import { FaTrash } from 'react-icons/fa';
+
+const TaskList = ({ tasks, removeTask, editTask }) => {
   if (tasks.length === 0) {
     return <></>;
   }
 
   return (
     <>
-      <p className='display-6'>Gorevler:</p>
-      <ul className='list-group my-5'>
-        {tasks.map((item) => {
-          return (
-            <li
-              key={item.uuid}
-              className='list-group-item d-flex justify-content-between'
-            >
-              {item.task}
-              <span
-                className='btn btn-danger btn-sm'
-                onClick={() => removeTask(item.uuid)}
-              >
-                Sil
-              </span>
-            </li>
-          );
-        })}
-      </ul>
+      <div className='p-4 bg-light mb-5 border rounded'>
+        <h4>Gorevler:</h4>
+        <ul className='list-group'>
+          {tasks.map((item) => {
+            return (
+              <li key={item.uuid} className='list-group-item'>
+                {item.task}
+                <div className='btn-group float-end' role='group'>
+                  <button
+                    type='button'
+                    className='btn btn-sm btn-info'
+                    onClick={() => editTask(item.uuid)}
+                  >
+                    Duzenle
+                  </button>
+                  <button
+                    type='button'
+                    className='btn btn-sm btn-danger align-middle'
+                    onClick={() => removeTask(item.uuid)}
+                  >
+                    Sil
+                    <span className='ms-2'>
+                      <FaTrash />
+                    </span>
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
