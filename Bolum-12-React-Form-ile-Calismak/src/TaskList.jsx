986533de14073b6fbc0602ed1e7678c0cef7982e
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-11 01:25:40 +0300

    listeledigimiz datalar farkli componenta alindi

--- Bolum-12-React-Form-ile-Calismak/src/TaskList.jsx
+++ Bolum-12-React-Form-ile-Calismak/src/TaskList.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react';
-import { FaTrash, FaRocketchat } from 'react-icons/fa';
+import TaskListItem from './TaskListItem';
 
-const TaskList = ({ tasks, removeTask, editTask }) => {
+const TaskList = ({ tasks, removeTask, editTask, doneTask }) => {
   const [priority, setPriority] = useState(false);
   const [filteredTasks, setFilteredTasks] = useState(tasks);
 
   const handlePriorityFilter = () => {
-    setPriority((prev) => !prev);
+    const newPriority = !priority;
+    newPriority
+      ? setFilteredTasks(tasks.filter((item) => item.priority))
+      : setFilteredTasks(tasks);
+    setPriority(newPriority);
   };
 
   useEffect(() => {
     setFilteredTasks(tasks);
   }, [tasks]);
 
-  useEffect(() => {
-    priority
-      ? setFilteredTasks(tasks.filter((item) => item.priority))
-      : setFilteredTasks(tasks);
-  }, [priority]);
+  // useEffect(() => {
+  //   priority
+  //     ? setFilteredTasks(tasks.filter((item) => item.priority))
+  //     : setFilteredTasks(tasks);
+  // }, [priority]);
 
   if (tasks.length === 0) {
     return <></>;
@@ -28,45 +32,25 @@ const TaskList = ({ tasks, removeTask, editTask }) => {
       <div className='p-4 bg-light mb-5 border rounded'>
         <h4 className='mb-3'>
           Gorevler:
-          <span
+          <button
             className={`btn btn-sm ${
               priority ? 'btn-primary' : 'btn-warning'
             } float-end`}
             onClick={handlePriorityFilter}
           >
             {priority ? 'Hepsini Goster' : 'Oncelikli Olanlari Goster'}
-          </span>
+          </button>
         </h4>
         <ul className='list-group'>
           {filteredTasks.map((item) => {
             return (
-              <li key={item.uuid} className='list-group-item'>
-                {item.priority && (
-                  <span className='badge text-bg-secondary me-2'>
-                    <FaRocketchat />
-                  </span>
-                )}
-                {item.task}
-                <div className='btn-group float-end' role='group'>
-                  <button
-                    type='button'
-                    className='btn btn-sm btn-info'
-                    onClick={() => editTask(item.uuid)}
-                  >
-                    Duzenle
-                  </button>
-                  <button
-                    type='button'
-                    className='btn btn-sm btn-danger align-middle'
-                    onClick={() => removeTask(item.uuid)}
-                  >
-                    Sil
-                    <span className='ms-2'>
-                      <FaTrash />
-                    </span>
-                  </button>
-                </div>
-              </li>
+              <TaskListItem
+                key={item.uuid}
+                {...item}
+                removeTask={removeTask}
+                editTask={editTask}
+                doneTask={doneTask}
+              />
             );
           })}
         </ul>
