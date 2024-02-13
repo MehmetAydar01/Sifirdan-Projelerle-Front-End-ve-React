ac4187ad318ae650e0f8670ef520e77b0390f888
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-13 23:26:38 +0300

    Page not found eklendi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/pages/index.js
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as Contact } from './Contact';
 export { default as About } from './About';
 export { default as Login } from './Login';
+export { default as PageNotFound } from './PageNotFound';
