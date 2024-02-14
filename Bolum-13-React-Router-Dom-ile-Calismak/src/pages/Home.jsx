05b1594d3199377e429ba979c6c5ad55933dd21a
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-14 22:11:37 +0300

    Anasayfada urunler getirilmesi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/pages/Home.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+import ProductCard from '../products/ProductCard';
+
 const Home = () => {
+  const [last10Products, setLast10Products] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=10')
+      .then((res) => res.json())
+      .then((res) => setLast10Products(res));
+  }, []);
+
   return (
     <>
       <h1>Ana Sayfa</h1>
+      <h2>Urunler:</h2>
+      <div className='row row-cols-sm-3 row-cols-md-4'>
+        {last10Products.map((item) => {
+          return <ProductCard key={item.id} {...item} />;
+        })}
+      </div>
     </>
   );
 };
