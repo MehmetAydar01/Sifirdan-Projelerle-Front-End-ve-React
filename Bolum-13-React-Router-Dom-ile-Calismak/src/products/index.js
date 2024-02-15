7b60bef1093c4b0aad9c8427a173ebc8aacf4371
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-15 23:20:44 +0300

    products klasorundeki componentlar export edildi

---
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/index.js
@@ -0,0 +1,5 @@
+export { default as Category } from './Category';
+export { default as Product } from './Product';
+export { default as Products } from './Products';
+export { default as ProductCard } from './ProductCard';
+export { default as ProductsLayout } from './ProductsLayout';
