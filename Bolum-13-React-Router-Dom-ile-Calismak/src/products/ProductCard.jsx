a9f4d17f7189fa0a1063cbcab1be585f82c50737
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-15 23:23:09 +0300

    stil düzenlemesi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/ProductCard.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/ProductCard.jsx
@@ -4,7 +4,7 @@ const ProductCard = ({ id, title, price, description, category, image }) => {
       <div className='card'>
         <img
           src={image}
-          className='card-img-top'
+          className='card-img-top p-2 object-fit-cover'
           height={300}
           width={400}
           alt={title}
@@ -12,7 +12,7 @@ const ProductCard = ({ id, title, price, description, category, image }) => {
         <div className='card-body'>
           <h5 className='card-title'>{title} </h5>
           <p className='card-text'>{description.substring(0, 20)}..</p>
-          <p className='lead'>{price}</p>
+          <p className='fw-bold'>{price.toFixed(2)}$</p>
           <a href='#' className='btn btn-primary'>
             Fav
           </a>
