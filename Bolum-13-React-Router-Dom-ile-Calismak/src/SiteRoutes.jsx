206731704a86303227fa1493f64ba10aecfc3f3f
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-15 23:20:59 +0300

    Products Layout component route eklendi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/SiteRoutes.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/SiteRoutes.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { About, Contact, Home, PageNotFound } from './pages';
+import { Products, ProductsLayout } from './products';
 
 const SiteRoutes = () => {
   return (
@@ -7,6 +8,9 @@ const SiteRoutes = () => {
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
+      <Route path='/products' element={<ProductsLayout />}>
+        <Route index={true} element={<Products />} />
+      </Route>
       <Route path='*' element={<PageNotFound />} />
     </Routes>
   );
