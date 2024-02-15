1f8be12d244642142601159c56f82017dd48685b
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-15 23:22:58 +0300

    Categoriler getirildi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/Category.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/Category.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Category = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then((res) => res.json())
+      .then((res) => setCategories(res));
+  }, []);
+
   return (
     <>
-      <h1>Category</h1>
+      <h1>Kategoriler</h1>
+      <div className='list-group'>
+        {categories.map((category, index) => {
+          return (
+            <Link
+              key={index}
+              to={`/products/category/${category}`}
+              className='list-group-item list-group-item-action text-uppercase'
+            >
+              {category}
+            </Link>
+          );
+        })}
+      </div>
     </>
   );
 };
