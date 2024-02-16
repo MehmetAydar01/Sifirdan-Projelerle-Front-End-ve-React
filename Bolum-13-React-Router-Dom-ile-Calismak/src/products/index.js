4ac7c521b61ba7f1ac6c4ab075bb4150b6822c1f
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-16 23:09:21 +0300

    Kategori Detay Eklendi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/index.js
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/index.js
@@ -1,4 +1,5 @@
 export { default as Category } from './Category';
+export { default as Categories } from './Categories';
 export { default as Product } from './Product';
 export { default as Products } from './Products';
 export { default as ProductCard } from './ProductCard';
