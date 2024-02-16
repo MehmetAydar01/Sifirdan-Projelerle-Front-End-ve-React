809bb8fdb429b923c5c1b97692299c7ff0aba2d0
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-16 23:09:54 +0300

    Kategori Detay

---
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/Categories.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then((res) => res.json())
+      .then((res) => setCategories(res));
+  }, []);
+
+  return (
+    <>
+      <h1>Kategoriler</h1>
+      <div className='list-group'>
+        {categories.map((category, index) => {
+          return (
+            <Link
+              key={index}
+              to={`/products/category/${category}`}
+              className='list-group-item list-group-item-action text-uppercase'
+            >
+              {category}
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/Category.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/Category.jsx
@@ -1,29 +1,24 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import ProductCard from './ProductCard';
 
 const Category = () => {
-  const [categories, setCategories] = useState([]);
+  const { categoryName } = useParams();
+  const [categoryProducts, setCategoryProducts] = useState([]);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products/categories')
+    fetch(`https://fakestoreapi.com/products/category/${categoryName}`)
       .then((res) => res.json())
-      .then((res) => setCategories(res));
-  }, []);
+      .then((res) => setCategoryProducts(res))
+      .catch((err) => console.error(err));
+  }, [categoryName]);
 
   return (
     <>
-      <h1>Kategoriler</h1>
-      <div className='list-group'>
-        {categories.map((category, index) => {
-          return (
-            <Link
-              key={index}
-              to={`/products/category/${category}`}
-              className='list-group-item list-group-item-action text-uppercase'
-            >
-              {category}
-            </Link>
-          );
+      <h1>Kategori: {categoryName.toUpperCase()}</h1>
+      <div className='row row-cols-sm-3 row-cols-md-4'>
+        {categoryProducts.map((item) => {
+          return <ProductCard key={item.id} {...item} />;
         })}
       </div>
     </>
