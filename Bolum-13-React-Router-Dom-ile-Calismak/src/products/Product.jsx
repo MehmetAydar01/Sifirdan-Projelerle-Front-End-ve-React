3b22ef7e95029803c8741cffa231c367bad6d2f7
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-17 23:08:35 +0300

    urun detay sayfasi olusturuldu

--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/Product.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/Product.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 const Product = () => {
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${productId}`)
+      .then((res) => res.json())
+      .then((res) => setProduct(res))
+      .catch((err) => console.error(err));
+  }, [productId]);
+
   return (
     <>
-      <h1>Product</h1>
+      <h1>{product?.title}</h1>
+      <img src={product?.image} alt='' className='w-50 img-thumbnail' />
+      <p className='py-3'>{product?.description}</p>
     </>
   );
 };
