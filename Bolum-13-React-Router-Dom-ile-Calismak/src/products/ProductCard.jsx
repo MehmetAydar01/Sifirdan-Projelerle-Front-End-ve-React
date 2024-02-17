4ca5c29cb5b505588898a278783f5719d787a04c
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-02-17 23:08:51 +0300

    img'lar link component icine alindi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/ProductCard.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/ProductCard.jsx
@@ -1,14 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const ProductCard = ({ id, title, price, description, category, image }) => {
+  const productUrl = `/products/product/${id}`;
   return (
     <div className='col-sm mb-3'>
       <div className='card'>
-        <img
-          src={image}
-          className='card-img-top p-2'
-          height={300}
-          width={400}
-          alt={title}
-        />
+        <Link to={productUrl}>
+          <img
+            src={image}
+            className='card-img-top p-2'
+            height={300}
+            width={400}
+            alt={title}
+          />
+        </Link>
         <div className='card-body'>
           <h5 className='card-title'>{title} </h5>
           <p className='card-text'>{description.substring(0, 20)}..</p>
