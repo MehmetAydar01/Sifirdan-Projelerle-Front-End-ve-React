958667beb8bdec694ef55e6f39408d6c37dc4f6c
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-05-16 18:49:47 +0300

    oy kullanılmadigi takdirde gecerli kosul eklendi

--- Bolum-11-React/bolum-sonu-calismasi-v3/src/components/SelectionVote.jsx
+++ Bolum-11-React/bolum-sonu-calismasi-v3/src/components/SelectionVote.jsx
@@ -1,8 +1,16 @@
 const SelectionVote = ({ numberLists, getVoteValue }) => {
+  if (!getVoteValue) {
+    return (
+      <div className='get-selection-vote'>
+        <span>You did not select out of {numberLists.length}</span>
+      </div>
+    );
+  }
+
   return (
     <div className='get-selection-vote'>
       <span>
-        You Selected {getVoteValue} out of {numberLists.length}
+        You Selected {getVoteValue || 0} out of {numberLists.length}
       </span>
     </div>
   );
