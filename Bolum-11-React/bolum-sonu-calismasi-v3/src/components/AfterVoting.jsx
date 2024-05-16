2192cdbea31b833fb81ef65dafed6ab2b05bfcf1
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-05-16 18:40:05 +0300

    secim yaptiktan sonra gelen comp olusturuldu

---
+++ Bolum-11-React/bolum-sonu-calismasi-v3/src/components/AfterVoting.jsx
@@ -0,0 +1,22 @@
+import Icon from './Icon';
+import thankyouIcon from '../images/illustration-thank-you.svg';
+import Content from './Content';
+import SelectionVote from './SelectionVote';
+
+const AfterVoting = ({ numberLists, getVoteValue }) => {
+  const cardContent = {
+    cardTitle: 'Thank you!',
+    cardText:
+      'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!',
+  };
+
+  return (
+    <section className='after-vote'>
+      <Icon thankyouIcon={thankyouIcon} />
+      <SelectionVote numberLists={numberLists} getVoteValue={getVoteValue} />
+      <Content cardContent={cardContent} />
+    </section>
+  );
+};
+
+export default AfterVoting;
