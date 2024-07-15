0166b6196e8a2aa3722967810ab5ac56931f225b
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-07-15 03:46:51 +0300

    buton tiklaninca favorilere ekleme islemleri yapildi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/products/ProductCard.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/products/ProductCard.jsx
@@ -1,7 +1,32 @@
 import { Link } from 'react-router-dom';
 
-const ProductCard = ({ id, title, price, description, category, image }) => {
+const ProductCard = ({
+  id,
+  title,
+  price,
+  description,
+  category,
+  image,
+  user,
+}) => {
   const productUrl = `/products/product/${id}`;
+  const item = { id, title, price, description, image, category };
+
+  const handleClick = (product) => {
+    let localFavs = JSON.parse(localStorage.getItem('favs')) ?? [];
+    const itemIndex = localFavs.findIndex(
+      (localItem) => product.id === localItem.id
+    );
+
+    if (itemIndex >= 0) {
+      localFavs = localFavs.filter((item) => item.id !== product.id);
+    } else {
+      localFavs.push({ id: product.id, title: product.title });
+    }
+
+    localStorage.setItem('favs', JSON.stringify(localFavs));
+  };
+
   return (
     <div className='col-sm mb-3'>
       <div className='card'>
@@ -18,9 +43,11 @@ const ProductCard = ({ id, title, price, description, category, image }) => {
           <h5 className='card-title'>{title} </h5>
           <p className='card-text'>{description.substring(0, 20)}..</p>
           <p className='fw-bold'>{price.toFixed(2)}$</p>
-          <a href='#' className='btn btn-primary'>
-            Fav
-          </a>
+          {user && (
+            <a className='btn btn-primary' onClick={() => handleClick(item)}>
+              Fav
+            </a>
+          )}
         </div>
       </div>
     </div>
