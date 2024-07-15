af0a26df5cc38fa14d0335a6e9343d7eef953bb0
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-07-15 03:43:40 +0300

    login islemleri ve route comp eklendi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/App.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Navbar } from './components';
 import SiteRoutes from './SiteRoutes';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user')) ?? null);
+  }, []);
+
+  const handleLogin = () => {
+    const user = { id: 1, name: 'lorem' };
+    setUser(user);
+    localStorage.setItem('user', JSON.stringify(user));
+    navigate('/');
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+  };
 
   return (
     <>
-      <Navbar />
+      <Navbar user={user} handleLogout={handleLogout} />
       <div className='container'>
         <div className='row'>
           <div className='col-sm-12'>
-            <SiteRoutes />
+            <SiteRoutes handleLogin={handleLogin} user={user} />
           </div>
         </div>
       </div>
