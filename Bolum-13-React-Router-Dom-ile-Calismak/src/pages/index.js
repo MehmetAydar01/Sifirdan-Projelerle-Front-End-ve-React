499e8b01f4d64b881e14747ac58a03e31f07b02f
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-07-15 03:45:13 +0300

    Favoriler sayfasi export edildi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/pages/index.js
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/pages/index.js
@@ -2,4 +2,5 @@ export { default as Home } from './Home';
 export { default as Contact } from './Contact';
 export { default as About } from './About';
 export { default as Login } from './Login';
+export { default as Fav } from './Fav';
 export { default as PageNotFound } from './PageNotFound';
