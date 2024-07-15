790039e95689f1875a9e25cb5e2b74be45853800
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-07-15 03:43:57 +0300

    route yapisi hazirlandi

--- Bolum-13-React-Router-Dom-ile-Calismak/src/SiteRoutes.jsx
+++ Bolum-13-React-Router-Dom-ile-Calismak/src/SiteRoutes.jsx
@@ -1,18 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
-import { About, Contact, Home, PageNotFound } from './pages';
+import { About, Contact, Fav, Home, Login, PageNotFound } from './pages';
 import { Category, Product, Products, ProductsLayout } from './products';
+import PrivateRoute from './PrivateRoute';
 
-const SiteRoutes = () => {
+const SiteRoutes = ({ handleLogin, user }) => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home user={user} />} />
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/products' element={<ProductsLayout />}>
-        <Route index={true} element={<Products />} />
-        <Route path='category/:categoryName' element={<Category />} />
-        <Route path='product/:productId' element={<Product />} />
+        <Route index={true} element={<Products user={user} />} />
+        <Route
+          path='category/:categoryName'
+          element={<Category user={user} />}
+        />
+        <Route path='product/:productId' element={<Product user={user} />} />
       </Route>
+      <Route path='/login' element={<Login handleLogin={handleLogin} />} />
+      <Route
+        path='/fav'
+        element={
+          <PrivateRoute user={user}>
+            <Fav />
+          </PrivateRoute>
+        }
+      />
       <Route path='*' element={<PageNotFound />} />
     </Routes>
   );
